8efeb99c4cb5467a3879ee6c3e1e474e19e2918e
6010 <6010hom@gmail.com>
2022-03-31 12:22:26 +0200

    Added store file

---
+++ store.js
@@ -0,0 +1,6 @@
+import { writable } from 'svelte/store';
+
+export const bullets = writable(0);
+export const state = writable(0);
+export const kill = writable(0);
+export const earnings = writable(0);
